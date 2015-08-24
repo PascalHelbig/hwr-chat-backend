b96963435c3c1191c2ea8b863319b507172a5dab
Tommy90 <ajsdakj@hdhd.de>
2015-08-24 13:51:22 +0200

    close #10 Tabellen in Loopback angelegt

--- server/boot/sql-migrate.js
+++ server/boot/sql-migrate.js
@@ -4,4 +4,7 @@
 
 module.exports = function(app) {
   app.dataSources.mysqlServerPascal.automigrate('Account');
+  app.dataSources.mysqlServerPascal.automigrate('Message');
+  app.dataSources.mysqlServerPascal.automigrate('Course');
+  app.dataSources.mysqlServerPascal.automigrate('Chat');
 };
