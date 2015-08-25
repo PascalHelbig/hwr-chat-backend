29a8963b1fa529fa8803e7a43c9929f9b0aba203
Pascal Helbig <pascalhelbig@gmail.com>
2015-08-25 11:12:18 +0200

    Creating a user at migration close #14

--- server/boot/sql-migrate.js
+++ server/boot/sql-migrate.js
@@ -3,7 +3,10 @@
  */
 
 module.exports = function(app) {
-  app.dataSources.mysqlServerPascal.automigrate('Account');
+  app.dataSources.mysqlServerPascal.automigrate('Account', function(err) {
+    if (err) throw err;
+    app.models.Account.create({email:"test@hwr-berlin.de", password:"1234"});
+  });
   app.dataSources.mysqlServerPascal.automigrate('Message');
   app.dataSources.mysqlServerPascal.automigrate('Course');
   app.dataSources.mysqlServerPascal.automigrate('Chat');
