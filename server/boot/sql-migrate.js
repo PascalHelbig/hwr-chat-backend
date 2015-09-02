1985a8cc0e63c5132ec8335952eb788405948636
Pascal Helbig <pascalhelbig@gmail.com>
2015-09-02 10:32:25 +0200

    Für alle Tabellen Dummy Daten anlegen close #15

--- server/boot/sql-migrate.js
+++ server/boot/sql-migrate.js
@@ -3,12 +3,38 @@
  */
 
 module.exports = function(app) {
-  app.dataSources.mysqlServerPascal.automigrate('Account', function(err) {
+  var async = require('async');
+
+  app.dataSources.mysqlServerPascal.automigrate([
+    'Account',
+    'Message',
+    'Course',
+    'Chat',
+    'AccountChat'
+  ], function(err) {
     if (err) throw err;
-    app.models.Account.create({email:"test@hwr-berlin.de", password:"1234", firstname:"Test", lastname: "User"});
+    app.models.Course.create({name: "Informatik"}, function(err, course) {
+      if (err) throw err;
+      async.parallel([
+        function(callback) {
+          app.models.Account.create({email:"test@hwr-berlin.de", password:"1234", firstname:"Test", lastname: "User", courseId: course.id}, callback);
+        },
+        function(callback) {
+          app.models.Account.create({email:"test2@hwr-berlin.de", password:"1234", firstname:"Test2", lastname: "User2", courseId: course.id}, callback);
+        },
+        function(callback) {
+          app.models.Chat.create({name: "Gruppenchat 1"}, callback);
+        }
+      ], function(err, result) {
+        if (err) throw err;
+        user1 = result[0];
+        user2 = result[1];
+        chat = result[2];
+        app.models.AccountChat.create({accountId: user1.id, chatId: chat.id});
+        app.models.AccountChat.create({accountId: user2.id, chatId: chat.id});
+        app.models.Message.create({accountId: user1.id, chatId: chat.id, content: "Ich bin User 1 :)"});
+        app.models.Message.create({accountId: user2.id, chatId: chat.id, content: "Ich bin User 2 :p"});
+      });
+    });
   });
-  app.dataSources.mysqlServerPascal.automigrate('Message');
-  app.dataSources.mysqlServerPascal.automigrate('Course');
-  app.dataSources.mysqlServerPascal.automigrate('Chat');
-  app.dataSources.mysqlServerPascal.automigrate('AccountChat');
 };
