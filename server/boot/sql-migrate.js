5cf64470ac8a279838fb0297855d9dc3af03f7ae
Pascal Helbig <pascalhelbig@gmail.com>
2015-09-10 22:54:08 +0200

    erstellt 100 Dummy Accounts damit die Suche spannender ist ;)

--- server/boot/sql-migrate.js
+++ server/boot/sql-migrate.js
@@ -4,6 +4,8 @@
 
 module.exports = function(app) {
   var async = require('async');
+  var faker = require('faker');
+  faker.locale = "de";
 
   app.dataSources.mysqlServerPascal.automigrate([
     'Account',
@@ -15,6 +17,17 @@ module.exports = function(app) {
     if (err) throw err;
     app.models.Course.create({name: "Informatik"}, function(err, course) {
       if (err) throw err;
+      for (var i = 0; i < 100; i++) {
+        var firstName = faker.name.firstName();
+        var lastName = faker.name.lastName();
+        app.models.Account.create({
+          email: "s_" + lastName.toLowerCase() + "@hwr-berlin.de",
+          password: "1234",
+          firstname: firstName,
+          lastname: lastName,
+          courseId: course.id
+        });
+      }
       async.parallel([
         function(callback) {
           app.models.Account.create({email:"test@hwr-berlin.de", password:"1234", firstname:"Test", lastname: "User", courseId: course.id}, callback);
@@ -34,6 +47,7 @@ module.exports = function(app) {
         app.models.AccountChat.create({accountId: user2.id, chatId: chat.id});
         app.models.Message.create({accountId: user1.id, chatId: chat.id, content: "Ich bin User 1 :)"});
         app.models.Message.create({accountId: user2.id, chatId: chat.id, content: "Ich bin User 2 :p"});
+        console.log('migration finished')
       });
     });
   });
