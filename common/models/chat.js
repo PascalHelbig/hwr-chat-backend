dbc6dbad6972d5b5f966225eef3b8d77211b6e65
NikCas <s_casper@stud.hwr-berlin.de>
2015-10-21 19:38:49 +0200

    socket.io Beispiel adaptiert https://docs.strongloop.com/display/MSG/Building+a+real-time+app+using+socket.io+and+AngularJS

--- common/models/chat.js
+++ common/models/chat.js
@@ -1,3 +1,40 @@
-module.exports = function(Chat) {
+var pubsub = require('../../server/pubsub.js');
+var loopback = require('loopback');
 
+module.exports = function(Chat) {
+  //Chat after save..
+  Chat.observe('after save', function (ctx, next) {
+    var socket = Chat.app.io;
+    if(ctx.isNewInstance){
+      //Now publishing the data..
+      pubsub.publish(socket, {
+        collectionName : 'Chat',
+        data: ctx.instance,
+        method: 'POST'
+      });
+    }else{
+      //Now publishing the data..
+      pubsub.publish(socket, {
+        collectionName : 'Chat',
+        data: ctx.instance,
+        modelId: ctx.instance.id,
+        method: 'PUT'
+      });
+    }
+    //Calling the next middleware..
+    next();
+  }); //after save..
+  //ChatDetail before delete..
+  Chat.observe("before delete", function(ctx, next){
+    var socket = Chat.app.io;
+    //Now publishing the data..
+    pubsub.publish(socket, {
+      collectionName : 'Chat',
+      data: ctx.instance.id,
+      modelId: ctx.instance.id,
+      method: 'DELETE'
+    });
+    //move to next middleware..
+    next();
+  });
 };
