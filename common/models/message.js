33c4d720edd1d43b5691aa2006fe8700e57ddc83
pascalhelbig <pascalhelbig@gmail.com>
2016-01-05 14:11:28 +0100

    bei neuen Messages die AccountId aus dem Token beziehen close #25

--- common/models/message.js
+++ common/models/message.js
@@ -1,4 +1,17 @@
+var loopback = require('loopback');
 module.exports = function (Message) {
+
+  // Vor dem Speichern der Nachricht, setzte accountId auf den angemeldeten User
+  Message.observe('before save', function (ctx, next) {
+    var context = loopback.getCurrentContext();
+    var user = context && context.get('accessToken');
+
+    if (user) {
+      ctx.instance.accountId = user.userId;
+    }
+    next();
+  });
+
   Message.observe('after save', function (ctx, next) {
 
     if (ctx.isNewInstance) {
